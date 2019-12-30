cee605b1fddc838bd285dd52f3d7ab498dcc41a1
David Vivó <david.vivo@oramon.nl>
2019-12-30 13:04:53 +0100

    Create and apply persons component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import styled from 'styled-components';
 import classes from './App.css';
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
 
 // const StyledButton = styled.button `
 //   background-color: ${ props => props.personsAreShown ? 'red' : 'green' };
@@ -75,16 +75,10 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}>
-              <Person
-                click={this.deletePersonHandler.bind(this, index)}
-                // click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                changed={(event) => this.nameChangedHandler(event, person.id)} />
-            </ ErrorBoundary>
-          })}
+          <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler} />
         </div>
       );
 
