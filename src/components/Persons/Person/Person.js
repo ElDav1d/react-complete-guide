a848209aa47e14842c720c49cebaea035d572ecf
David Vivó <david.vivo@oramon.nl>
2019-12-31 11:42:58 +0100

    Remove wrapping div by auxiliary high order component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,26 @@
 import React,  { Component } from 'react';
-
+import Auxiliary from '../../../hoc/Auxiliary';
 import classes from './Person.css';
 
 class Person extends Component {
     render(){
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
+            <Auxiliary >                
                 <p onClick={this.props.click}>
                     I'm {this.props.name} and I'm {this.props.age} years old!
                 </p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            </Auxiliary>
+            
+            // <div className={classes.Person}>
+            //     <p onClick={this.props.click}>
+            //         I'm {this.props.name} and I'm {this.props.age} years old!
+            //     </p>
+            //     <p>{this.props.children}</p>
+            //     <input type="text" onChange={this.props.changed} value={this.props.name} />
+            // </div>
         );
     }
 }
