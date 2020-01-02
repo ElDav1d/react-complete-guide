6d8cf5ff1ee8e53deb1feaa6b06bcdc7780585e2
David Vivó <david.vivo@oramon.nl>
2020-01-02 19:21:01 +0100

    Apply higher order component  to person and spread unknown props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,21 @@
 import React,  { Component } from 'react';
 import classes from './Person.css';
+import Auxiliary from '../../../hoc/Auxiliary';
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
     render(){
         console.log('[Person.js] rendering...');
         return (            
-            <div className={classes.Person}>
+            <Auxiliary >
                 <p onClick={this.props.click}>
                     I'm {this.props.name} and I'm {this.props.age} years old!
                 </p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            </Auxiliary>
         );
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
