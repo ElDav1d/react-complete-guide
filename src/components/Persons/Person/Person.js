6f04c3e847f4f6407b694dab8b311deb7d92a1ff
David Vivó <david.vivo@oramon.nl>
2020-01-04 12:33:24 +0100

    Apply Ref to focus on last rendered person component's input.
    Only valid with class based components. This is the newer approach.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,8 +5,14 @@ import withClass from '../../../hoc/withClass';
 import PropTypes from 'prop-types';
 
 class Person extends Component {
+    constructor() {
+        super();
+        this.inputElementRef = React.createRef();
+    }
+
     componentDidMount() {
-        this.inputElement.focus();
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus()
     }
     
     render(){
@@ -19,7 +25,8 @@ class Person extends Component {
                 </p>
                 <p>{this.props.children}</p>
                 <input
-                    ref={(inputEl) => {this.inputElement = inputEl}}
+                    // ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
                     type="text"
                     onChange={this.props.changed}
                     value={this.props.name}
